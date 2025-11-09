8916602062b2722e889dfa4bd2b4fcf01b5d9eb5
JavadAg <j.aghebati93@gmail.com>
2025-11-10 01:54:51 +0330

    refactor: update Google sign-in

--- app/components/auth/GoogleSignIn.vue
+++ app/components/auth/GoogleSignIn.vue
@@ -7,29 +7,20 @@ const supabase = useSupabaseClient()
 
 const signInWithGoogle = async () => {
   const { error: googleError } = await supabase.auth.signInWithOAuth({
-    provider: 'google',
+    provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/confirm`
+      redirectTo: `${globalThis.location.origin}/confirm`
     }
   })
 
   if (googleError) {
-    emit('error', googleError?.message || 'An unexpected error occurred')
-    console.error(
-      'Google sign-in error:',
-      googleError?.message || 'An unexpected error occurred'
-    )
+    emit("error", googleError?.message || "An unexpected error occurred")
+    console.error("Google sign-in error:", googleError?.message || "An unexpected error occurred")
   }
 }
 </script>
 <template>
-  <UButton
-    color="neutral"
-    variant="outline"
-    size="lg"
-    block
-    @click="signInWithGoogle"
-  >
+  <UButton color="neutral" variant="outline" size="lg" block @click="signInWithGoogle">
     <span
       class="text-xs size-5 bg-gradient-to-r from-blue-500 to-red-500 rounded-sm flex items-center justify-center font-bold text-inverted"
       >G</span
