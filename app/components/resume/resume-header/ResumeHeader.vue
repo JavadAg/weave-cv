eb4ab739adf6c61879557176fc86da0df3648446
JavadAg <j.aghebati93@gmail.com>
2025-11-16 23:31:53 +0330

    refactor: improved alignment and add padding

--- app/components/resume/resume-header/ResumeHeader.vue
+++ app/components/resume/resume-header/ResumeHeader.vue
@@ -16,17 +16,17 @@ const titleModel = computed({
 
 <template>
   <div class="relative flex items-center justify-between gap-4 p-4 rounded-xl bg-default">
-    <UButton
-      to="/dashboard"
-      variant="ghost"
-      color="neutral"
-      size="lg"
-      icon="i-lucide-arrow-left"
-      class="hidden lg:flex"
-    >
-      <span class="hidden xl:inline">Dashboard</span>
-    </UButton>
-    <div class="flex-1 min-w-0">
+    <div class="flex items-center gap-2">
+      <UButton
+        to="/dashboard"
+        variant="ghost"
+        color="neutral"
+        size="lg"
+        icon="i-lucide-arrow-left"
+        class="hidden lg:flex"
+      >
+        <span class="hidden xl:inline">Dashboard</span>
+      </UButton>
       <UInput
         v-model="titleModel"
         :minlength="3"
@@ -34,6 +34,9 @@ const titleModel = computed({
         placeholder="Resume Title"
         size="lg"
         icon="i-heroicons-document-text"
+        :ui="{
+          base: 'pe-11'
+        }"
       >
         <template #trailing>
           <span class="text-xs text-muted">{{ titleModel.length }}/50</span>
